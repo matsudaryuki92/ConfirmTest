5c52328292824ebd8c39749a414f03cdcfd3869b
ryuki <ryuuki.sub.9208@gmail.com>
2024-11-20 17:30:15 +0900

    ccc

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -16,29 +16,13 @@ class ContactController extends Controller
     {
         $contact = $request->only('first_name', 'last_name', 'gender', 'email', 'tel1', 'tel2', 'tel3','address', 'building', 'category_id', 'detail');
 
-        $genderCheck = [
-            1 => '男性',
-            2 => '女性',
-            3 => 'その他',
-        ];
-
-         $category_name = [
-            1 => '商品のお届けについて',
-            2 => '商品の交換について',
-            3 => '商品トラブル',
-            4 => 'ショップへのお問い合わせ',
-            5 => 'その他',
-        ];
-
-        $contact['gender'] = $genderCheck[$contact['gender']] ?? '未設定';
-        $contact['category_id'] = $category_name[$contact['category_id']] ?? '未設定';
-        $contact['tel'] = $request->tel1 . '-' . $request->tel2 . '-' . $request->tel3;;
         return view('confirm', compact('contact'));
     }
 
     public function store(Request $request)
     {
         $contact = $request->only('first_name', 'last_name', 'gender', 'email', 'tel','address', 'building', 'category_id', 'detail');
+        $contact['tel'] = $request->tel1 . '-' . $request->tel2 . '-' . $request->tel3;;
         Contact::create($contact);
         return view('thanks');
     }
--- src/app/Models/Contact.php
+++ src/app/Models/Contact.php
@@ -10,15 +10,19 @@ class Contact extends Model
     use HasFactory;
 
     protected $fillable = [
+        'category_id',
         'first_name', 
         'last_name', 
         'gender', 
         'email', 
-        'tel1',
-        'tel2',
-        'tel3',
+        'tel',
         'address', 
         'building', 
         'detail'
     ];
+
+    public function category()
+    {
+        return $this->belogsTo(Category::class);
+    }
 }
